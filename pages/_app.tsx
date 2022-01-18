e0cc9ada7ca335128359b0028a35b49c4c054f8e
dzaaaaaa <dzaaaaaa@outlook.com>
2022-01-18 22:00:13 +0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-import IconList from "../components/IconList";
 
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
-        <>
-            <Component {...pageProps} />
-            <IconList/>
-        </>
+        <Component {...pageProps} />
     );
 }
 
