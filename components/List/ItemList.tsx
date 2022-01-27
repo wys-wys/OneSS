f82f5951f4aeda8c439955c44d82add70e1e4315
dzaaaaaa <dzaaaaaa@outlook.com>
2022-01-27 23:36:33 +0800

    fix

--- components/List/ItemList.tsx
+++ components/List/ItemList.tsx
@@ -14,7 +14,7 @@ export default function ItemList({user, route, data}: { user: string, route?: st
                         <div key={index} className={"group flex flex-row space-x-2 hover:bg-white hover:bg-opacity-10 border-b border-gray-500"}>
                             <Folder32Regular/>
                             <Link href={`/${user}/${route ? route.slice(-1) + '/' : ''}${name}`}>
-                                <a className={"basis-full text-xl flex items-center hover:bg-white hover:bg-opacity-30 truncate"}>{name}</a>
+                                <a className={"basis-5/6 text-xl flex items-center hover:bg-white hover:bg-opacity-30 truncate"}>{name}</a>
                             </Link>
                             <div className={"basis-1/12 flex items-center justify-center"}>{`${ConvertB(size)}`}
                             </div>
@@ -27,9 +27,9 @@ export default function ItemList({user, route, data}: { user: string, route?: st
                              className={"group flex flex-row space-x-2 hover:bg-white hover:bg-opacity-10 border-b border-gray-500"}>
                             <Document32Regular/>
                             <Link href={`/item/${user}/${id}`}>
-                                <a className={"basis-full text-xl flex items-center hover:bg-white hover:bg-opacity-30 truncate"}>{name}</a>
+                                <a className={"basis-5/6 text-xl flex items-center hover:bg-white hover:bg-opacity-30 truncate"}>{name}</a>
                             </Link>
-                            <div className={"basis-1/12 flex items-center justify-center invisible md:visible"}>{`${ConvertB(size)}`}
+                            <div className={"basis-1/12 flex items-center justify-center"}>{`${ConvertB(size)}`}
                             </div>
                             <div className={"basis-1/12 flex flex-row justify-center items-center invisible group-hover:visible"}>
                                 <a href={dl} className={"hover:bg-white hover:bg-opacity-10"} download><ArrowDownload24Regular/></a>
@@ -45,11 +45,11 @@ export default function ItemList({user, route, data}: { user: string, route?: st
 
 function ConvertB(B: number) {
     let size: string
-    if (B < 0.1 * 1024) {
+    if (B < 1024) {
         size = B.toFixed(2) + "B";
-    } else if (B < 0.1 * 1024 * 1024) {
+    } else if (B < 1024 * 1024) {
         size = (B / 1024).toFixed(2) + "KB";
-    } else if (B < 0.1 * 1024 * 1024 * 1024) {
+    } else if (B < 1024 * 1024 * 1024) {
         size = (B / (1024 * 1024)).toFixed(2) + "MB";
     } else {
         size = (B / (1024 * 1024 * 1024)).toFixed(2) + "GB";
