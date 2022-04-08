b14f4c70df73afa3c2371c6d025dc36ca8879587
Tualin14 <t@dzaaaaaa.com>
2022-04-08 17:48:15 +0800

    取消可以返回200文件以上选择，影响性能

--- src/pages/api/children.ts
+++ src/pages/api/children.ts
@@ -6,48 +6,15 @@ import getToken from "@/script/get_token";
 import baseSetting from "@/setting/baseSetting";
 
 
-let totalData: any[] = []
-
 const children = async (req: { query: { user: string, route?: string, thumbnails?: boolean } }, res: NextApiResponse<itemType[]>) => {
     const {'user': user, 'route': route, 'thumbnails': thumbnails = false} = req.query
-    const accessToken = await getToken()
-    const data = await getChildrenByRoute(user, route ? `/${route}` : '', thumbnails, accessToken)
-
-    if (data.status == 233) {
-        res.status(200).json(data)
-    } else if (baseSetting.over200) {
-        await getNextData(data, accessToken)
-        if (totalData) {
-            const resData = data.value.concat(totalData)
-            totalData = []
-            res.status(200).json(resData)
-        } else {
-            const resData = data.value
-            totalData = []
-            res.status(200).json(resData)
-        }
-    } else {
-        res.status(200).json(data.value)
-    }
+    const data = await getChildrenByRoute(user, route ? `/${route}` : '', thumbnails)
+    res.status(200).json(data)
 }
 export default children
 
-async function getNextData(data: any, accessToken: string) {
-    const nextUrl = data['@odata.nextLink']
-    if (nextUrl) {
-        const res = await axios.get(nextUrl, {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            },
-        })
-        totalData = totalData.concat(res.data.value)
-        await getNextData(res.data, accessToken)
-    } else {
-        return totalData
-    }
-}
-
-async function getChildrenByRoute(user: string, route: string = '', thumbnails: boolean, accessToken: string) {
+async function getChildrenByRoute(user: string, route: string = '', thumbnails: boolean) {
+    const accessToken = await getToken()
     const url = encodeURI(`${baseSetting.endpoints.graph_endpoint}/users/${user}/drive/root:${baseSetting.folder}${route}:/children`)
 
     try {
@@ -60,7 +27,7 @@ async function getChildrenByRoute(user: string, route: string = '', thumbnails: 
                 select: 'name,size,id,folder,file,image,video',
             },
         })
-        return res.data
+        return res.data.value
     } catch (e) {
         return {status: 233}
     }
